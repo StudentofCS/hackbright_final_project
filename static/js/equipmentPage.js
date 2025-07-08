239b922419a703ac4cc0f7d5e8705ddab2112e86
Luis Minaya <luis.minaya@gmail.com>
2025-07-08 18:55:40 -0400

    Working on adding AJAX to the homepage search.

--- static/js/equipmentPage.js
+++ static/js/equipmentPage.js
@@ -821,3 +821,92 @@ if (window.location.pathname.startsWith('/build/')) {
             }
         });
 }
+
+
+if (window.location.pathname == ('/')) {
+
+    const search_button = document.querySelector('#homepage_submit_button')
+
+    search_button.addEventListener('click', update_search_results)
+}
+
+
+function update_search_results(evt) {
+    evt.preventDefault();
+
+
+
+
+    const formInputs = {
+        build_name: document.querySelector(
+            'input[name="build_name"]').value,
+        character_class_ids : Array.from(document.querySelectorAll(
+            'input[name="character_class_id"]')).map(element => {
+                if (element.checked) {
+                    return element.value
+                }
+            }).filter(element => typeof element !== 'undefined'),
+        character_class_ids : Array.from(document.querySelectorAll(
+            'input[name="character_class_id"]')),
+        character_class_ids : Array.from(document.querySelectorAll(
+            'input[name="character_class_id"]')),
+        min_level: document.querySelector('input[name="min_level"]').value,
+        max_level: document.querySelector('input[name="max_level"]').value,
+        min_hp: document.querySelector('input[name="min_hp"]').value,
+        max_hp: document.querySelector('input[name="max_hp"]').value,
+        min_ap: document.querySelector('input[name="min_ap"]').value,
+        max_ap: document.querySelector('input[name="max_ap"]').value,
+        min_mp: document.querySelector('input[name="min_mp"]').value,
+        max_mp: document.querySelector('input[name="max_mp"]').value,
+        min_wp: document.querySelector('input[name="min_wp"]').value,
+        max_wp: document.querySelector('input[name="max_wp"]').value,
+        min_elemental_mastery: document.querySelector(
+            'input[name="min_elemental_mastery"]').value,
+        max_elemental_mastery: document.querySelector(
+            'input[name="max_elemental_mastery"]').value,
+        min_elemental_res: document.querySelector('input[name="min_elemental_res"]').value,
+        max_elemental_res: document.querySelector('input[name="max_elemental_res"]').value,
+        mastery_element: document.querySelectorAll('input[name="min_level"]').value,
+        res_element: document.querySelectorAll('input[name="max_level"]').value,
+        min_crit_hit: document.querySelector('input[name="min_crit_hit"]').value,
+        max_crit_hit: document.querySelector('input[name="max_crit_hit"]').value,
+        min_block: document.querySelector('input[name="min_block"]').value,
+        max_block: document.querySelector('input[name="max_block"]').value,
+        min_initiative: document.querySelector('input[name="min_initiative"]').value,
+        max_initiative: document.querySelector('input[name="max_initiative"]').value,
+        min_spell_range: document.querySelector('input[name="min_spell_range"]').value,
+        max_spell_range: document.querySelector('input[name="max_spell_range"]').value,
+        min_dodge: document.querySelector('input[name="min_dodge"]').value,
+        max_dodge: document.querySelector('input[name="max_dodge"]').value,
+        min_lock: document.querySelector('input[name="min_lock"]').value,
+        max_lock: document.querySelector('input[name="max_lock"]').value,
+        min_wisdom: document.querySelector('input[name="min_wisdom"]').value,
+        max_wisdom: document.querySelector('input[name="max_wisdom"]').value,
+        min_prospecting: document.querySelector('input[name="min_prospecting"]').value,
+        max_prospecting: document.querySelector('input[name="max_prospecting"]').value,
+        min_control: document.querySelector('input[name="min_control"]').value,
+        max_control: document.querySelector('input[name="max_control"]').value,
+        min_force_of_will: document.querySelector('input[name="min_force_of_will"]').value,
+        max_force_of_will: document.querySelector('input[name="max_force_of_will"]').value,
+        min_crit_mastery: document.querySelector('input[name="min_crit_mastery"]').value,
+        max_crit_mastery: document.querySelector('input[name="max_crit_mastery"]').value,
+        min_crit_res: document.querySelector('input[name="min_crit_res"]').value,
+        max_crit_res: document.querySelector('input[name="max_crit_res"]').value,
+        min_rear_mastery: document.querySelector('input[name="min_rear_mastery"]').value,
+        max_rear_mastery: document.querySelector('input[name="max_rear_mastery"]').value,
+        min_rear_res: document.querySelector('input[name="min_rear_res"]').value,
+        max_rear_res: document.querySelector('input[name="max_rear_res"]').value,
+        min_melee_mastery: document.querySelector('input[name="min_melee_mastery"]').value,
+        max_melee_mastery: document.querySelector('input[name="max_melee_mastery"]').value,
+        min_armor_given: document.querySelector('input[name="min_armor_given"]').value,
+        max_armor_given: document.querySelector('input[name="max_armor_given"]').value,
+        min_distance_mastery: document.querySelector('input[name="min_distance_mastery"]').value,
+        max_distance_mastery: document.querySelector('input[name="max_distance_mastery"]').value,
+        min_armor_received: document.querySelector('input[name="min_armor_received"]').value,
+        max_armor_received: document.querySelector('input[name="max_armor_received"]').value,
+        min_healing_mastery: document.querySelector('input[name="min_healing_mastery"]').value,
+        max_healing_mastery: document.querySelector('input[name="max_healing_mastery"]').value,
+        min_berserk_mastery: document.querySelector('input[name="min_berserk_mastery"]').value,
+        max_berserk_mastery: document.querySelector('input[name="max_berserk_mastery"]').value,
+    };
+}
